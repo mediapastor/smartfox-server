5eb7d291c854a2468223707969e2613f0013e855
timlien <timlientw@gmail.com>
2016-02-22 20:18:26 +0800

    Update pom

---
+++ smartfox-server/src/main/java/com/fugu/test/smartfox_server/LoginEventHandler.java
@@ -0,0 +1,19 @@
+package com.fugu.test.smartfox_server;
+
+import com.smartfoxserver.v2.core.ISFSEvent;
+import com.smartfoxserver.v2.core.SFSEventParam;
+import com.smartfoxserver.v2.exceptions.SFSException;
+import com.smartfoxserver.v2.exceptions.SFSLoginException;
+import com.smartfoxserver.v2.extensions.BaseServerEventHandler;
+
+public class LoginEventHandler extends BaseServerEventHandler
+{
+    @Override
+    public void handleServerEvent(ISFSEvent event) throws SFSException
+    {
+        String name = (String) event.getParameter(SFSEventParam.LOGIN_NAME);
+         
+        if (name.equals("Gonzo") || name.equals("Kermit"))
+            throw new SFSLoginException("Gonzo and Kermit are not allowed in this Zone!");
+    }
+}
--- smartfox-server/src/main/java/com/fugu/test/smartfox_server/MyExtension.java
+++ smartfox-server/src/main/java/com/fugu/test/smartfox_server/MyExtension.java
@@ -1,13 +1,19 @@
 package com.fugu.test.smartfox_server;
 
+import com.smartfoxserver.v2.core.SFSEventType;
 import com.smartfoxserver.v2.extensions.SFSExtension;
 
 public class MyExtension extends SFSExtension {
 	
+	@Override
 	public void init() {
-		this.addRequestHandler("math", MathHandler.class);
-		// TODO Auto-generated method stub
+		trace("Hello, this is my first SFS2X Extension!");
 		
+		// Add a new Request Handler
+		addRequestHandler("math", MathHandler.class);
+
+		// Add a new SFSEvent Handler
+	    addEventHandler(SFSEventType.USER_LOGIN, LoginEventHandler.class);
 	}
 	
 	@Override
