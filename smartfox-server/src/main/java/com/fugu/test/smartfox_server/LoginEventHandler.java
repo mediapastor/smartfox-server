2b4feeb4cfee73f9dd031c1ce8945072d290e178
Tim Lien <timlientw@gmail.com>
2016-02-26 01:20:29 +0800

    Update crypt password login handler

--- smartfox-server/src/main/java/com/fugu/test/smartfox_server/LoginEventHandler.java
+++ smartfox-server/src/main/java/com/fugu/test/smartfox_server/LoginEventHandler.java
@@ -1,5 +1,6 @@
 package com.fugu.test.smartfox_server;
 
+import com.smartfoxserver.bitswarm.sessions.ISession;
 import com.smartfoxserver.v2.core.ISFSEvent;
 import com.smartfoxserver.v2.core.SFSEventParam;
 import com.smartfoxserver.v2.exceptions.SFSException;
@@ -12,12 +13,15 @@ public class LoginEventHandler extends BaseServerEventHandler
     public void handleServerEvent(ISFSEvent event) throws SFSException
     {
         String username = (String) event.getParameter(SFSEventParam.LOGIN_NAME); 
-        String password = (String) event.getParameter(SFSEventParam.LOGIN_PASSWORD);
+        String cryptPassword = (String) event.getParameter(SFSEventParam.LOGIN_PASSWORD);
+        ISession session = (ISession)event.getParameter(SFSEventParam.SESSION);
+        
+        ;
          
-        if (username.equals("tim") && password.equals("1234")) {
-        	System.out.printf("Login success with username %s and password %s.%n", username, password);
+        if (username.equals("tim") && getApi().checkSecurePassword(session, "1234", cryptPassword)) {
+        	System.out.printf("Login success with username %s and password %s.%n", username, cryptPassword);
         } else {
-        	throw new SFSLoginException("Access denied! Wrong username or password!");
+        	throw new SFSLoginException("Access denied! Wrong username or password! username:" + username + " cryptPasswordStatus:" + getApi().checkSecurePassword(session, "1234", cryptPassword));
         }
             
     }
